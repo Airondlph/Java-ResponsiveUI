f17a346e93a1fcbd300355ff419decee421f62cc
Airondlph <52699504+Airondlph@users.noreply.github.com>
2024-08-09 18:23:51 +0200

    Update module-info of ResponsiveUI module

--- Source Code/ResponsiveUI/src/ResponsiveUI/module-info.java
+++ Source Code/ResponsiveUI/src/ResponsiveUI/module-info.java
@@ -4,5 +4,11 @@
  * 
  */
 module ResponsiveUI {
+	requires java.desktop;
+	
 	exports com.airondlph.ui.responsive;
+	exports com.airondlph.ui.responsive.data;
+	exports com.airondlph.ui.responsive.component;
+	exports com.airondlph.ui.responsive.factory;
+	exports com.airondlph.ui.responsive.layout;
 }
